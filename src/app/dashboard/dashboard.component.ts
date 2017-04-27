0989dc79581b43d478f8322be4f4ccf375f800b5
Shubham Rai <raishubham1992@gmail.com>
2017-04-27 23:00:18 +0530

    Added table for fixed header

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -12,4 +12,128 @@ export class DashboardComponent implements OnInit {
   ngOnInit() {
   }
 
+  dummyJSON = [
+  {
+    "_id": "590222e235527c415d8e9814",
+    "index": 0,
+    "guid": "53355161-eff6-43f9-82ea-c4df4b593ab4",
+    "isActive": false,
+    "balance": "$1,403.28",
+    "picture": "http://placehold.it/32x32"
+  },
+  {
+    "_id": "590222e2516f27e68fd7a3c2",
+    "index": 1,
+    "guid": "1d75a594-47eb-4bc1-8d15-930ae7cfef5a",
+    "isActive": true,
+    "balance": "$1,416.13",
+    "picture": "http://placehold.it/32x32"
+  },
+  {
+    "_id": "590222e20d5b3d40676560db",
+    "index": 2,
+    "guid": "908ca4ff-c259-42ec-9341-e61c23958521",
+    "isActive": false,
+    "balance": "$3,244.97",
+    "picture": "http://placehold.it/32x32"
+  },
+  {
+    "_id": "590222e24490346991770ede",
+    "index": 3,
+    "guid": "d469d7ef-0dc8-4103-892f-d0c6a832bbf9",
+    "isActive": true,
+    "balance": "$3,332.68",
+    "picture": "http://placehold.it/32x32"
+  },
+  {
+    "_id": "590222e2a33640945db17ed5",
+    "index": 4,
+    "guid": "de113e47-c133-4567-b89d-76603f221f70",
+    "isActive": true,
+    "balance": "$2,238.58",
+    "picture": "http://placehold.it/32x32"
+  },
+  {
+    "_id": "590222e2ab6c986d3199be16",
+    "index": 5,
+    "guid": "f1c74df9-d1a4-4240-9967-623059dff537",
+    "isActive": true,
+    "balance": "$1,388.76",
+    "picture": "http://placehold.it/32x32"
+  },
+  {
+    "_id": "590222e25d80b6478b166da5",
+    "index": 6,
+    "guid": "3f23a04c-d602-4e06-8729-5cbb507db891",
+    "isActive": false,
+    "balance": "$1,928.58",
+    "picture": "http://placehold.it/32x32"
+  },
+  {
+    "_id": "590222e2420d6ec4b055a817",
+    "index": 7,
+    "guid": "a4a021ab-b580-40b3-a98d-9317ae258388",
+    "isActive": false,
+    "balance": "$1,042.55",
+    "picture": "http://placehold.it/32x32"
+  },
+  {
+    "_id": "590222e2866de9fb37b55f73",
+    "index": 8,
+    "guid": "dd4da6e1-2636-4aef-b370-a0407b87f890",
+    "isActive": true,
+    "balance": "$3,254.73",
+    "picture": "http://placehold.it/32x32"
+  },
+  {
+    "_id": "590222e22d6ecf630e8151a0",
+    "index": 9,
+    "guid": "62eef3a4-b81b-4fa2-9817-44e185caa3c8",
+    "isActive": false,
+    "balance": "$2,708.86",
+    "picture": "http://placehold.it/32x32"
+  },
+  {
+    "_id": "590222e2f8cebbfeefa315fb",
+    "index": 10,
+    "guid": "efa64d08-c1cc-4a8e-9c9b-435f62bb1b61",
+    "isActive": true,
+    "balance": "$3,509.80",
+    "picture": "http://placehold.it/32x32"
+  },
+  {
+    "_id": "590222e2940c34ff22ea054f",
+    "index": 11,
+    "guid": "dab58a2f-55fe-42c2-b2d6-358f9cf1b11e",
+    "isActive": true,
+    "balance": "$2,164.40",
+    "picture": "http://placehold.it/32x32"
+  },
+  {
+    "_id": "590222e2bba74787e872a162",
+    "index": 12,
+    "guid": "3019433e-0cce-43e7-a0ff-217f1d6afed6",
+    "isActive": false,
+    "balance": "$2,000.51",
+    "picture": "http://placehold.it/32x32"
+  },
+  {
+    "_id": "590222e24b57341cb8a96e3c",
+    "index": 13,
+    "guid": "cdf12896-620d-4699-ab21-b14da8b97546",
+    "isActive": true,
+    "balance": "$2,959.59",
+    "picture": "http://placehold.it/32x32"
+  },
+  {
+    "_id": "590222e2d31a315a30914bdd",
+    "index": 14,
+    "guid": "eace3187-1420-42b6-9d23-fc1973885be7",
+    "isActive": true,
+    "balance": "$2,101.57",
+    "picture": "http://placehold.it/32x32"
+  }
+]
+  
+
 }
